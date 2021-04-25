51ac454ee3af22b517f296fa9a0d7e3fa7161bac
kmecpp <kmecpp@gmail.com>
2021-04-25 16:45:32 -0400

    7,721 errors left yay

--- src/main/java/icbm/classic/prefab/FakeRadioSender.java
+++ src/main/java/icbm/classic/prefab/FakeRadioSender.java
@@ -1,75 +1,67 @@
 package icbm.classic.prefab;
 
+import icbm.classic.ICBMClassic;
 import icbm.classic.api.tile.IRadioWaveReceiver;
 import icbm.classic.api.tile.IRadioWaveSender;
-import icbm.classic.ICBMClassic;
-import icbm.classic.lib.transform.region.Cube;
 import icbm.classic.lib.radio.RadioRegistry;
-import net.minecraft.entity.player.EntityPlayer;
+import icbm.classic.lib.transform.region.Cube;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 /**
  * Wrapper used by items to act as a radio wave sender
- *
- *
+ * <p>
+ * <p>
  * Created by Dark(DarkGuardsman, Robert) on 4/24/2016.
  */
-public class FakeRadioSender implements IRadioWaveSender
-{
-    public final EntityPlayer player;
-    public final ItemStack item;
-    Cube cube;
+public class FakeRadioSender implements IRadioWaveSender {
 
-    public FakeRadioSender(EntityPlayer player, ItemStack item, int range)
-    {
-        this.player = player;
-        this.item = item;
-        this.cube = new Cube(-range, Math.max(-range, 0), -range, range, range, range).add(player.posX, player.posY, player.posZ);
-    }
+	public final PlayerEntity player;
+	public final ItemStack item;
+	Cube cube;
 
-    @Override
-    public void onMessageReceived(IRadioWaveReceiver receiver, float hz, String header, Object[] data)
-    {
-        if (ICBMClassic.runningAsDev)
-        {
-            //player.addChatComponentMessage(new TextComponentString("Received message with header " + header + " on band " + hz + "hz with data " + data));
-        }
-    }
+	public FakeRadioSender(PlayerEntity player, ItemStack item, int range) {
+		this.player = player;
+		this.item = item;
+		this.cube = new Cube(-range, Math.max(-range, 0), -range, range, range, range).add(player.getPosX(), player.getPosY(), player.getPosZ());
+	}
 
-    @Override
-    public void sendRadioMessage(float hz, String header, Object... data)
-    {
-        RadioRegistry.popMessage(world(), this, hz, header, data);
-    }
+	@Override
+	public void onMessageReceived(IRadioWaveReceiver receiver, float hz, String header, Object[] data) {
+		if (ICBMClassic.runningAsDev) {
+			//player.addChatComponentMessage(new TextComponentString("Received message with header " + header + " on band " + hz + "hz with data " + data));
+		}
+	}
 
-    @Override
-    public Cube getRadioSenderRange()
-    {
-        return cube;
-    }
+	@Override
+	public void sendRadioMessage(float hz, String header, Object... data) {
+		RadioRegistry.popMessage(world(), this, hz, header, data);
+	}
 
-    @Override
-    public double x()
-    {
-        return player.posX;
-    }
+	@Override
+	public Cube getRadioSenderRange() {
+		return cube;
+	}
 
-    @Override
-    public double y()
-    {
-        return player.posY;
-    }
+	@Override
+	public double x() {
+		return player.getPosX();
+	}
 
-    @Override
-    public double z()
-    {
-        return player.posZ;
-    }
+	@Override
+	public double y() {
+		return player.getPosY();
+	}
 
-    @Override
-    public World world()
-    {
-        return player.world;
-    }
+	@Override
+	public double z() {
+		return player.getPosZ();
+	}
+
+	@Override
+	public World world() {
+		return player.world;
+	}
+
 }
